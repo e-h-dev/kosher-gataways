5e24ce068504f22187c204091545f083f7feed7d
e-h-dev <ebaal8226@gmail.com>
2025-06-12 14:37:01 +0100

    Update rental models

--- kosher_getaways/rentals/models.py
+++ kosher_getaways/rentals/models.py
@@ -16,13 +16,21 @@ class Rentals(models.Model):
     class Meta:
         verbose_name_plural = "Rentals"
 
+    owner_name = models.CharField(max_length=100, null=True, blank=True)
+    owner_number = models.IntegerField(default=0)
+    owner_email = models.EmailField(null=True, blank=True)
+    owner_address = models.CharField(max_length=100, null=True, blank=True)
+    owner_post_code = models.CharField(max_length=100, null=True, blank=True)
     location = models.ForeignKey('Location', null=True, blank=True,
                                  on_delete=models.SET_NULL)
-    name = models.CharField(max_length=254)
+    house_name = models.CharField(max_length=254, null=True, blank=True)
+    title = models.TextField(max_length=100)
+    sleeps = models.IntegerField(default=2)
+    bedrooms = models.IntegerField(default=1)
+    bathrooms = models.IntegerField(default=1)
+    amenities = models.CharField(max_length=1254, null=True, blank=True)
     description = models.TextField()
-    mini_description = models.TextField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    # image = models.ImageField(null=True, blank=True)
     available_from = models.DateField()
     available_till = models.DateField()
     rating = models.IntegerField(default=1,
@@ -37,6 +45,7 @@ class Image(models.Model):
     name = models.ForeignKey('Rentals', null=True, blank=True,
                              on_delete=models.CASCADE, related_name='images')
     image = models.ImageField(null=True, blank=True)
+    image_name = models.CharField(max_length=24, null=True, blank=True)
    
     def __str__(self):
         return f'Image for {self.name.name}'
